3bf35f622c94ade66412db70b0b1e1ac4307466c
promise <pebuka351@gmail.com>
2023-05-19 08:27:28 +0100

    c

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,6 +7,7 @@ export default function Profile() {
   const navigate = useNavigate();
   const [userDetails, setUserDetails] = useState();
   const [isCreated, setIsCreated] = useState(false);
+  const [modal, setModal] = useState(false);
 
   useEffect(() => {
     const getAccount = account.get();
@@ -36,17 +37,66 @@ export default function Profile() {
       {isCreated ? (
         <main>
           {userDetails ? (
-            <section className="w-full h-screen box-border px-[150px] bg-[hsl(255,100%,99%)]">
-              <ProfileNavbar />
-              <p
-                style={{
-                  fontFamily: "'EB Garamond', serif",
-                  fontFamily: "'Outfit', sans-serif",
-                }}
-                className="font-semibold text-xl text-[rgba(0,0,0,0.7)]"
+            <section className="w-full h-screen box-border pl-[150px] bg-[hsl(255,100%,99%)] max-[870px]:pl-5">
+              <div
+                style={{ display: `${modal === true ? "flex" : "none"}` }}
+                className="absolute w-full h-screen bg-[rgba(0,0,0,0.1)] left-0 justify-center items-center"
               >
-                Hello👋 {userDetails.name}
-              </p>
+                <div className="bg-[hsl(255,100%,99%)] w-60 h-36 rounded box-border py-4 px-4 flex flex-col justify-between">
+                  <p className="text-[15px] font-semibold text-[rgba(0,0,0,0.7)]">
+                    Are you sure <br /> you want to logout?
+                  </p>
+                  <div className="flex justify-between w-full">
+                    <button
+                      onClick={() => setModal(false)}
+                      className="w-[45%] bg-[rgba(0,0,0,0.2)] flex justify-center items-center rounded h-8 text-xs hover:scale-105"
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      onClick={handleLogout}
+                      className="w-[45%] bg-red-500 text-white flex justify-center items-center rounded h-8 text-xs hover:scale-105"
+                    >
+                      Logout
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <ProfileNavbar firstLetter={userDetails.name[0]} />
+              <section
+                style={{ gridTemplateColumns: "43% 57%" }}
+                className="grid justify-between w-full pb-[30px] max-[870px]:pb-4 max-[870px]:flex-col max-[870px]:flex"
+              >
+                <div className="pt-[70px]">
+                  <p
+                    style={{
+                      fontFamily: "'EB Garamond', serif",
+                      fontFamily: "'Outfit', sans-serif",
+                    }}
+                    className="pb-[2px] leading-[-1px] font-bold text-[50px] text-[hsl(260,8%,14%)]"
+                  >
+                    URL <br />
+                    Shortening
+                  </p>
+                  <p
+                    style={{
+                      fontFamily: "'EB Garamond', serif",
+                      fontFamily: "'Outfit', sans-serif",
+                    }}
+                    className="text-base text-[hsla(257,27%,26%,0.6)] duration-500 font-medium leading-[25px] pb-5"
+                  >
+                    Shorten the length of your long URL.
+                    <br /> And view all shortened links
+                  </p>
+                </div>
+                <div className="h-[300px] max-[870px]:h-[150px] max-[870px]:w-[92%] ">
+                  <img
+                    src="/images/illustration-working.svg"
+                    alt=""
+                    className="w-full"
+                  />
+                </div>
+              </section>
             </section>
           ) : (
             <section></section>
@@ -60,7 +110,7 @@ export default function Profile() {
 }
 
 const spinner = (
-  <main className="w-full h-screen flex justify-center items-center">
+  <main className="w-full h-screen flex justify-center items-center bg-[hsl(255,100%,99%)]">
     <img src="/images/spinner.svg" alt="" className="h-[60px]" />
   </main>
 );
--- src/components/ProfileNavbar.jsx
+++ src/components/ProfileNavbar.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function ProfileNavbar() {
+export default function ProfileNavbar({ firstLetter }) {
   return (
-    <nav className="w-full flex py-[30px] justify-between items-center">
-      <div className="links">
+    <nav className="w-full flex pr-[150px] py-[30px] justify-between items-center max-[870px]:pr-5">
+      <div className="">
         <h1 className="logo">Shortly</h1>
-        <div className="nav-ul">
-          <ul className="nav-ul-links">
-            <li>
-              <Link>Link Shortening</Link>
-            </li>
-          </ul>
-
-          <div className="self-center flex items-center gap-[2px] cursor-pointer">
-            <i class="fa-regular fa-circle-user text-lg text-[hsla(257,27%,26%,0.7)]"></i>
-            <i className="fa-solid fa-caret-down text-[hsla(257,27%,26%,0.7)] text-xs"></i>
-          </div>
+      </div>
+      <div className="self-center cursor-pointer flex items-center gap-[2px]">
+        <div
+          style={{
+            backgroundColor: `${colors[Math.floor(Math.random() * 8)]}`,
+          }}
+          className="w-[26px] h-[26px] rounded-[13px] flex justify-center items-center"
+        >
+          <p className="text-white text-sm font-semibold">{firstLetter}</p>
         </div>
+        <i className="fa-solid fa-caret-down text-xs text-[hsla(257,27%,26%,0.7)]"></i>
       </div>
     </nav>
   );
 }
+
+const colors = [
+  "#98343a",
+  "blue",
+  "purple",
+  "black",
+  "#851921",
+  "#69b024",
+  "#177d6a",
+  "#9b2397",
+];
