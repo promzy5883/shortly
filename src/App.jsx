a8e3b547f1edcc345fc6f3449300b857ab2437a7
promise <pebuka351@gmail.com>
2023-05-23 11:44:45 +0100

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import SignUp from "./components/SignUp";
 import Landing from "./landing";
+import { useEffect } from "react";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
-import { MemoryRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <MemoryRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
-    </MemoryRouter>
+    </BrowserRouter>
   );
 }
 
